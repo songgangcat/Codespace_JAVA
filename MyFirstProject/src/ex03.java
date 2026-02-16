8b0de6957a279e725ef344b8744cf7a045e038ad
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-02-16 07:08:43 +0000

    ex03_switch

---
+++ MyFirstProject/src/ex03.java
@@ -0,0 +1,14 @@
+public class ex03 {
+    public static void main(String[] args) {
+        int x = 1;
+        switch (x) {
+            case 1:
+                System.out.println("정답입니다.");
+                break;
+        
+            default:
+                System.out.println("오답입니다");
+                break;
+        }
+    }
+}
