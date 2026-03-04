90122d48c5a1c2972f5e2c50b6319d8ab08bf00c
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-03-04 10:59:50 +0000

    ex08_array print

---
+++ MyFirstProject/src/ex08.java
@@ -0,0 +1,14 @@
+//배열의 출력
+import java.util.Arrays;
+
+class ex08 {
+    public static void main(String[] args) {
+        int[] iArr = {100,95,80,70,60};
+        
+        for(int i = 0;i<iArr.length;i++) {
+            System.out.println(iArr[i]);
+        }
+        
+        System.out.println(Arrays.toString(iArr));
+    }
+}
