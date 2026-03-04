6e7430421c0894ef86cffbfc1ffaf9aacf8e38d7
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-03-04 10:59:35 +0000

    ex06_array length

---
+++ MyFirstProject/src/ex07.java
@@ -0,0 +1,15 @@
+// 배열의 길이
+
+public class ex07 {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        System.out.println("arr.length=" + arr.length);
+        
+
+
+        for(int i=0;i<arr.length;i++) {
+            System.out.println("arr[" + i + "]=" + arr[i]);
+        }
+ 
+    }
+}
