01112b787a3c5ae25845610b505fac2f818b282a
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-03-04 10:58:47 +0000

    ex06_array

---
+++ MyFirstProject/src/ex06.java
@@ -0,0 +1,18 @@
+//배열
+
+public class ex06 {
+    public static void main(String[] args) {
+        int[] score = new int[5]; // 배열 생성 및 선언
+        score[3] = 100;
+
+        System.out.println("score[0]=" + score[0]);
+        System.out.println("score[1]=" + score[1]);
+        System.out.println("score[2]=" + score[2]);
+        System.out.println("score[3]=" + score[3]);
+        System.out.println("score[4]=" + score[4]);    
+
+        int value = score[3];
+        System.out.println("value=" + value);
+    }
+
+}
