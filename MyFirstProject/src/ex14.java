98f681e840a5d7e3098caedcbabedfb847328d33
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-03-05 14:53:40 +0000

    ex14_command line input

---
+++ MyFirstProject/src/ex14.java
@@ -0,0 +1,11 @@
+// 명령 라인 입력받기 -> 입력받은 변수들 하나씩 출력
+ 
+
+class ex14 {
+    public static void main(String[] args) {
+        System.out.println("매개변수의 개수:" + args.length);
+        for(int i=0;i < args.length;i++){
+            System.out.println("args[" + i + "] = \""+ args[i] + "\"");
+        }
+    }
+}
