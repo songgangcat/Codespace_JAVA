94cf31e36395c5cea04ae9363cced384860c1ccd
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-03-05 17:09:16 +0000

    ex15_multiple arrays utilize 2

---
+++ MyFirstProject/src/ex16.java
@@ -0,0 +1,41 @@
+//2차원 배열(2) - 총점과 평균
+
+public class ex16 {
+    public static void main(String[] args) {
+        int[][] score = {
+            {100, 100, 100},
+            {20, 20, 20},
+            {30, 30, 30},
+            {40, 40, 40}
+        };
+        
+        int korTotal = 0, engTotal = 0, mathTotal = 0;
+
+        System.out.println("번호 국어 영어 수학 총점 평균");
+        System.out.println("===============================");
+
+        for(int i=0;i < score.length;i++) {
+            int sum = 0; // 개인별 총점
+            float avg = 0f; // 개인별 평균
+
+            korTotal += score[i][0]; // 같은 과목의 총합 추가
+            engTotal += score[i][0];
+            mathTotal += score[i][0];
+            System.out.printf("%3d", i+1);
+            
+            for(int j=0;j < score[i].length;j++) {
+                sum += score[i][j];
+                System.out.printf("%5d", score[i][j]);
+            }
+
+            avg = sum/(float)score[i].length; // 평균
+            System.out.printf("%5d %5.1f%n", sum, avg);
+
+
+        }
+
+        System.out.println("===============================");
+        System.out.printf("총점:%3d %5d %4d%n", korTotal, engTotal, mathTotal);
+    }
+    
+}
