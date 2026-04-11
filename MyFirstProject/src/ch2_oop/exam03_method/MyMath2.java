a378d81b2e3f4c4944bcdeee0ccbfa0615112e96
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-04-11 10:27:27 +0000

    ch2_exam3_instance OR class method

---
+++ MyFirstProject/src/ch2_oop/exam03_method/MyMath2.java
@@ -0,0 +1,19 @@
+package ch2_oop.exam03_method;
+
+public class MyMath2 {
+    long a, b; // iv
+
+    // iv a, b만을 활용하여 작업 -> 매개변수가 필요 X
+    // 아래 4개는 인스턴스 메서드
+    long add() {return a+b;} // a, b는 iv
+    long subtrack() {return a-b;}
+    long multiply() {return a*b;}
+    double divide() {return a/b;}
+
+    // 인스턴스 변수와 관계없이 매개변수만으로 작업이 가능하다.
+    // 아래 4개는 클래스 메서드
+    static long add(long a, long b) { return a+b; } // a, b는 지역변수
+    static long substract(long a, long b) { return a-b; }
+    static long multiply(long a, long b) { return a*b; }
+    static double divide(double a, double b) { return a/b; }
+}
