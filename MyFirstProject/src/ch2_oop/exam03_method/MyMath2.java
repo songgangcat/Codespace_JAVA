e03e74f32d5f7a451e2416f0386f25628cb52c89
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-04-11 10:32:20 +0000

    rebuild

--- MyFirstProject/src/ch2_oop/exam03_method/MyMath2.java
+++ MyFirstProject/src/ch2_oop/exam03_method/MyMath2.java
@@ -16,4 +16,16 @@ public class MyMath2 {
     static long substract(long a, long b) { return a-b; }
     static long multiply(long a, long b) { return a*b; }
     static double divide(double a, double b) { return a/b; }
+
+    
+    public static void main(String[] args) {
+        //클래스 메서드 호출 -> 인스턴스 생성 없이 호출 가능
+        System.out.println(MyMath2.add(200L, 100L));
+        //인스턴스 메서드 호출 -> 먼저 객체 생성 후
+        MyMath2 mm = new MyMath2();
+        mm.a = 200L;
+        mm.b = 100L;
+        System.out.println(mm.add());
+
+    }
 }
