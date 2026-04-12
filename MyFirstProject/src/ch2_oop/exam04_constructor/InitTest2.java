d842cd9885ec382b301749b991502202de0b51db
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-04-12 07:33:54 +0000

    ch2_exam4_InitTest2

---
+++ MyFirstProject/src/ch2_oop/exam04_constructor/InitTest2.java
@@ -0,0 +1,16 @@
+package ch2_oop.exam04_constructor;
+
+class InitTest2 {
+
+		//명시적 초기화
+		static int cv = 1;
+		int iv = 1; 
+		
+		static {    cv = 2;   } // 클래스 초기화 블럭
+
+        {   iv = 2;   } // 인스턴스 초기화 블럭
+
+        InitTest2 () { // 생성자
+            iv = 3;
+        }
+}
