b1e3de6a55cfe8bd763b659039421477653d848b
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-04-12 07:15:07 +0000

    ch2_exam4_InitTest

---
+++ MyFirstProject/src/ch2_oop/exam04_constructor/InitTest.java
@@ -0,0 +1,12 @@
+package ch2_oop.exam04_constructor;
+
+public class InitTest {
+    int x; // 인스턴스 변수. 자동 초기화
+    int y = x; // 인스턴스 변수. 자동 초기화
+
+    void method1() {
+      //int i; // 지역변수. 수동 초기화
+      //int j = i; //에러 -> 지역변수를 초기화하지 않고 사용
+    }
+    
+}
